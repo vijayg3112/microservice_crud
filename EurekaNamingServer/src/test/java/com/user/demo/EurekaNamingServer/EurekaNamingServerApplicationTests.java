5f51eb2f34cd0f1cab96d0d37c9483dd2f6a5f4a
Vanita Manseta <Vanita Manseta@Lenovo0235.asl.mum.sst>
2019-11-27 13:27:26 +0530

    Eureka commit

---
+++ EurekaNamingServer/src/test/java/com/user/demo/EurekaNamingServer/EurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.demo.EurekaNamingServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
